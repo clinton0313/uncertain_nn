7301a214469f3eae6a12e1614bdaacf0428ba2f4
clinton0313 <clinton0313@gmail.com>
2022-02-26 10:55:22 +0100

    Deleted checkpoints

--- code/basic_dropout.py
+++ code/basic_dropout.py
@@ -17,6 +17,7 @@ tf.random.set_seed(1234)
 DATA_DIR = pathlib.Path("horse_or_human")
 LABELS = {0: "horse", 1: "human"}
 CHECKPOINT_PATH = "checkpoints"
+os.makedirs(CHECKPOINT_PATH, exist_ok=True)
 
 #%%
 #Load the data
@@ -42,8 +43,6 @@ val_ds = ds["validate"].cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 #Using Data Augmentation
 
 train_datagen = ImageDataGenerator(
-    # featurewise_center=True,
-    # featurewise_std_normalization=True,
     rescale=(1./255),
     zoom_range=0.2,
     rotation_range=30,
@@ -74,24 +73,26 @@ val_generator = val_datagen.flow_from_directory(
 # %%
 #Specify model
 models = [
-    # CNNDropout(num_classes=2, p_dropout=0.5), 
-    # CNNDropout(num_classes =2, p_dropout=0.7),
-    CNNDropConnect(num_classes=2, p_dropout=0.3)
+    CNNDropout(num_classes=2, p_dropout=0.5), 
+    CNNDropout(num_classes =2, p_dropout=0.7),
+    CNNDropConnect(num_classes=2, p_dropout=0.7),
+    CNNDropConnect(num_classe=2, p_dropout=0.9)
     ]
 
 modelnames = [
-    # "dropout_p5", 
-    # "dropout_p7", 
-    "dropconnect_p3"
+    "dropout_p5", 
+    "dropout_p7", 
+    "dropconnect_p7"
+    "dropconnect_p9"
     ]
 checkpoints = [ModelCheckpoint(os.path.join(CHECKPOINT_PATH, name), save_weights_only=True, save_best_only=True, monitor="val_loss") for name in modelnames]
 
 #%%
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, name="crossentropy")
-early_stopping = EarlyStopping(patience=15, restore_best_weights=True, monitor="val_loss")
-lr_scheduler = ReduceLROnPlateau(factor=0.5, patience=4, min_lr = 1e-5, verbose=1)
+early_stopping = EarlyStopping(patience=20, restore_best_weights=True, monitor="val_loss")
+lr_scheduler = ReduceLROnPlateau(factor=0.5, patience=4, min_lr = 1e-8, verbose=1)
 
 #%%
 #Compile and fit
@@ -109,8 +110,8 @@ for model, checkpoint, name in zip(models, checkpoints, modelnames):
         print("No weights to load, starting to fit instead...")
 
 
-    history = model.fit(train_ds, 
-        validation_data = val_ds, 
+    history = model.fit(train_generator, 
+        validation_data = val_generator, 
         epochs = 100, 
         callbacks = [checkpoint, early_stopping, lr_scheduler],
         verbose = 1)
