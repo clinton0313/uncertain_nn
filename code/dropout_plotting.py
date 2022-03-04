3c8a64613956a6f11a67fb508355496771dbfb86
clinton0313 <clinton0313@gmail.com>
2022-03-04 09:12:12 +0100

    Reorganized plotting file and separated helper functions.

--- code/dropout_plotting.py
+++ code/dropout_plotting.py
@@ -1,24 +1,20 @@
 #%%
 
-import itertools
 import matplotlib.pyplot as plt
-import numpy as np
 import os
-import pandas as pd
 import pathlib
 import pickle
 import PIL
 import random
-import regex as re
-from tqdm import tqdm
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+
+from helper_functions import evaluate_measures, plot_history, plot_measures, load_measures
 from HH_CNN import CNNDropConnect, CNNDropout, CNNPlotter
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 #Global Variables
-tf.random.set_seed(1234)
 DATA_DIR = pathlib.Path("horse_or_human")
 LABELS = {0: "horse", 1: "human"}
 CHECKPOINT_PATH = "checkpoints"
@@ -26,54 +22,6 @@ SAVEPATH = "figs"
 modelnames = ["dropout_p5", "dropout_p7", "dropconnect_p3", "dropconnect_p5"]
 os.makedirs(SAVEPATH, exist_ok=True)
 
-#Helper Function
-
-def plot_history(history, cols, title=""):
-    results = pd.DataFrame(history)
-    results = results.loc[:, cols]
-    fig, ax = plt.subplots(figsize=(14,14))
-    ax = results.plot(ax=ax)
-    ax.set_title(title)
-    ax.grid(True)
-    return fig
-
-def evaluate_measures(SAVEPATH, model_titles, plotters, use_mc, datasets, dataset_names, 
-                      out_sample, wrong_color, max_batches=5, T = 10):
-    for (title, plotter, mc) in zip(model_titles, plotters, use_mc):
-        for dataset_name, dataset, out, color in zip(dataset_names, datasets, out_sample, wrong_color):
-            loader = iter(dataset)
-            plotter.reset_measures()
-            for _ in tqdm(range(min(max_batches, len(dataset)))):
-                images, labels = next(loader)
-                plotter.gather_measures(images, labels, out_sample=out, mc=mc, T=T)
-            with open (os.path.join(SAVEPATH, f"{title}_{dataset_name}_measures.pkl"), "wb") as outfile:
-                pickle.dump((plotter.correct_scores, plotter.wrong_scores), outfile)
-            print(f"Measures saved for {title} {dataset_name}")
-            wrong_label = dataset_name if out else "Wrong"
-            if out:
-                new_fig = False
-            else:
-                new_fig = True
-            plotter.plot_measures(title=f"{title}", out_sample=out, wrong_color=color, 
-                                  wrong_label=wrong_label, new_fig=new_fig)
-        model_title = title.replace(" ", "_")
-        plotter.save_fig(os.path.join(SAVEPATH, f"{model_title.lower()}_eval.png"))
-
-
-#%%
-#Plot some horses and some humans
-humans = list(DATA_DIR.glob('*/humans/*'))
-horses = list(DATA_DIR.glob('*/horses/*'))
-horse_sample = [PIL.Image.open(str(horse)) for horse in random.sample(horses, 8)]
-human_sample = [PIL.Image.open(str(human)) for human in random.sample(humans, 8)]
-
-cnn_plotter = CNNPlotter()
-cnn_plotter.plot_batch(horse_sample, 2, 4, figsize=(8, 16))
-cnn_plotter.fig
-
-#%%
-cnn_plotter.plot_batch(human_sample, 2, 4, figsize=(8, 16))
-cnn_plotter.fig
 #%%
 #Load the data
 batch_size = 16
@@ -117,48 +65,20 @@ mc_dropconnect.compile(optimizer=optimizer, loss=loss_fn)
 mc_dropout.load_weights(os.path.join(CHECKPOINT_PATH, modelnames[0]))
 mc_dropconnect.load_weights(os.path.join(CHECKPOINT_PATH, modelnames[2]))
 
+#%%
+#Plot some horses and some humans
+humans = list(DATA_DIR.glob('*/humans/*'))
+horses = list(DATA_DIR.glob('*/horses/*'))
+horse_sample = [PIL.Image.open(str(horse)) for horse in random.sample(horses, 8)]
+human_sample = [PIL.Image.open(str(human)) for human in random.sample(humans, 8)]
+
+cnn_plotter = CNNPlotter()
+cnn_plotter.plot_batch(horse_sample, 2, 4, figsize=(8, 16))
+cnn_plotter.fig
 
 #%%
-#Plot our training losses
-
-with open(os.path.join(CHECKPOINT_PATH, f"{modelnames[0]}_history.pkl"), "rb") as infile:
-    dropout_hist = pickle.load(infile)
-
-with open(os.path.join(CHECKPOINT_PATH, f"{modelnames[2]}_history.pkl"), "rb") as infile:
-    dropconnect_hist = pickle.load(infile)
-
-#Plot our losses
-
-dropout_acc_fig = plot_history(dropout_hist, ["accuracy", "val_accuracy"], "MC Dropout with p = 0.5")
-dropconnect_acc_fig = plot_history(dropconnect_hist, ["accuracy", "val_accuracy"], "MC Dropconnect with p = 0.3")
-
-dropout_acc_fig.savefig(os.path.join(SAVEPATH, "dropout_acc.png"), facecolor="white", transparent=False)
-dropconnect_acc_fig.savefig(os.path.join(SAVEPATH, "dropconnect_acc.png"), facecolor="white", transparent=False)
-#%%
-
-fig_files = os.listdir(SAVEPATH)
-
-pkl_filter = re.compile(r'^.*pkl')
-
-pkl_files = list(filter(pkl_filter.search, fig_files))
-
-models = ["softmax", "dropout", "dropconnect"]
-datasets = ["horse", "cat", "flower"]
-scores = ["correct", "wrong"]
-
-measures = {}
-
-for m in models:
-    m_filter = re.compile(fr'^.*(?i){m}.*')
-    m_files = list(filter(m_filter.search, pkl_files))
-    for d in datasets:
-        d_filter = re.compile(fr'^.*(?i){d}.*')
-        d_files = list(filter(d_filter.search, m_files))
-        with open (os.path.join(SAVEPATH, d_files[0]), "rb") as infile:
-            correct, wrong = pickle.load(infile)
-        measures[f"{m}_{d}_correct"] = correct
-        measures[f"{m}_{d}_wrong"] = wrong
-
+cnn_plotter.plot_batch(human_sample, 2, 4, figsize=(8, 16))
+cnn_plotter.fig
 
 #%%
 #Plot some predictions!
@@ -187,37 +107,90 @@ for m in models:
 # cnn_plotter.plot_batch(images, 4, 4, predict=True, mc=True, T=100)
 # flowers_fig = cnn_plotter.fig
 
-# %%
-model_titles = ["Softmax", "MC Dropout", "MC DropConnect"]
-use_mc = [False, True, True]
-plotters = [
-    CNNPlotter(mc_dropout, labels=LABELS),
-    CNNPlotter(mc_dropout, labels=LABELS),
-    CNNPlotter(mc_dropconnect, labels=LABELS)
-]
-datasets = [
-    val_ds, 
-    catsdogs["train"],
-    flowers["train"]
-]
-dataset_names = [
-    "Horse and Human",
-    "Cats and Dogs",
-    "Flowers"
-]
-out_sample = [False, True, True]
-wrong_color = ["tab:red", "tab:brown", "tab:olive"]
 
-evaluate_measures(
-    SAVEPATH, 
-    model_titles=["MC DropConnect"], 
-    plotters=[CNNPlotter(mc_dropconnect, labels=LABELS)], 
-    use_mc=[True], 
-    datasets=datasets, 
-    dataset_names=dataset_names, 
-    out_sample=out_sample, 
-    wrong_color=wrong_color,
-    max_batches=16,
-    T=100,
-)
+#%%
+#Plot our training losses
 
+with open(os.path.join(CHECKPOINT_PATH, f"{modelnames[0]}_history.pkl"), "rb") as infile:
+    dropout_hist = pickle.load(infile)
+
+with open(os.path.join(CHECKPOINT_PATH, f"{modelnames[2]}_history.pkl"), "rb") as infile:
+    dropconnect_hist = pickle.load(infile)
+
+#Plot our losses
+
+dropout_acc_fig = plot_history(dropout_hist, ["accuracy", "val_accuracy"], "MC Dropout with p = 0.5")
+dropconnect_acc_fig = plot_history(dropconnect_hist, ["accuracy", "val_accuracy"], "MC Dropconnect with p = 0.3")
+
+dropout_acc_fig.savefig(os.path.join(SAVEPATH, "dropout_acc.png"), facecolor="white", transparent=False)
+dropconnect_acc_fig.savefig(os.path.join(SAVEPATH, "dropconnect_acc.png"), facecolor="white", transparent=False)
+#%%
+
+#Load Prediction Measures
+
+models = ["softmax", "dropout", "dropconnect"]
+datasets = ["horse", "cat", "flower"]
+types = ["correct", "wrong"]
+colors = ["tab:blue", "tab:orange", "tab:purple", "tab:olive"]
+data_titles = {"horse": "Horse and Human", "cat": "Cats vs Dogs", "flower": "Flowers"}
+
+measures = load_measures(SAVEPATH, models, datasets)
+
+#%%
+#Plot Prediction Measures
+
+datset_figs = []
+for data in datasets:
+    scores = [f"{m}_{data}_wrong" for m in models]
+    labels = [m.capitalize() for m in models]
+    datset_figs.append(plot_measures(measures, scores, colors, labels, title=data_titles[data])) 
+
+comp_fig, axs = plt.subplots(3, 1, figsize=(15, 15), tight_layout=True, sharey=False, sharex=True)
+for ax, model in zip(axs.ravel(), models):
+    plot_measures(
+        measures, 
+        scores=[f"{model}_horse_{t}" for t in types], 
+        colors=colors, 
+        labels=[t.capitalize() for t in types], 
+        ax=ax, 
+        title=model.capitalize()
+    )
+comp_fig.text(x=0.35, y = 3.35, s="Horse and Human Classification", fontsize=20, transform=ax.transAxes)
+
+
+#%%
+
+#Measure Evaluation
+
+# model_titles = ["Softmax", "MC Dropout", "MC DropConnect"]
+# use_mc = [False, True, True]
+# plotters = [
+#     CNNPlotter(mc_dropout, labels=LABELS),
+#     CNNPlotter(mc_dropout, labels=LABELS),
+#     CNNPlotter(mc_dropconnect, labels=LABELS)
+# ]
+# datasets = [
+#     val_ds, 
+#     catsdogs["train"],
+#     flowers["train"]
+# ]
+# dataset_names = [
+#     "Horse and Human",
+#     "Cats and Dogs",
+#     "Flowers"
+# ]
+# out_sample = [False, True, True]
+# wrong_color = ["tab:red", "tab:brown", "tab:olive"]
+
+# evaluate_measures(
+#     SAVEPATH, 
+#     model_titles=["MC DropConnect"], 
+#     plotters=[CNNPlotter(mc_dropconnect, labels=LABELS)], 
+#     use_mc=[True], 
+#     datasets=datasets, 
+#     dataset_names=dataset_names, 
+#     out_sample=out_sample, 
+#     wrong_color=wrong_color,
+#     max_batches=16,
+#     T=100,
+# )
---
+++ code/helper_functions.py
@@ -0,0 +1,77 @@
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import pickle
+import regex as re
+import seaborn as sns
+from tqdm import tqdm
+
+
+def plot_history(history, cols, title=""):
+    results = pd.DataFrame(history)
+    results = results.loc[:, cols]
+    fig, ax = plt.subplots(figsize=(14,14))
+    ax = results.plot(ax=ax)
+    ax.set_ylim(0,1.1)
+    ax.set_title(title)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    return fig
+
+def evaluate_measures(SAVEPATH, model_titles, plotters, use_mc, datasets, dataset_names, 
+                      out_sample, wrong_color, max_batches=5, T = 10):
+    for (title, plotter, mc) in zip(model_titles, plotters, use_mc):
+        for dataset_name, dataset, out, color in zip(dataset_names, datasets, out_sample, wrong_color):
+            loader = iter(dataset)
+            plotter.reset_measures()
+            for _ in tqdm(range(min(max_batches, len(dataset)))):
+                images, labels = next(loader)
+                plotter.gather_measures(images, labels, out_sample=out, mc=mc, T=T)
+            with open (os.path.join(SAVEPATH, f"{title}_{dataset_name}_measures.pkl"), "wb") as outfile:
+                pickle.dump((plotter.correct_scores, plotter.wrong_scores), outfile)
+            print(f"Measures saved for {title} {dataset_name}")
+            wrong_label = dataset_name if out else "Wrong"
+            if out:
+                new_fig = False
+            else:
+                new_fig = True
+            plotter.plot_measures(title=f"{title}", out_sample=out, wrong_color=color, 
+                                  wrong_label=wrong_label, new_fig=new_fig)
+        model_title = title.replace(" ", "_")
+        plotter.save_fig(os.path.join(SAVEPATH, f"{model_title.lower()}_eval.png"))
+
+def load_measures(SAVEPATH, models, datasets):
+    fig_files = os.listdir(SAVEPATH)
+    pkl_filter = re.compile(r'^.*pkl')
+    pkl_files = list(filter(pkl_filter.search, fig_files))
+    measures = {}
+    for m in models:
+        m_filter = re.compile(fr'^.*(?i){m}.*')
+        m_files = list(filter(m_filter.search, pkl_files))
+        for d in datasets:
+            d_filter = re.compile(fr'^.*(?i){d}.*')
+            d_files = list(filter(d_filter.search, m_files))
+            with open (os.path.join(SAVEPATH, d_files[0]), "rb") as infile:
+                correct, wrong = pickle.load(infile)
+            measures[f"{m}_{d}_correct"] = correct
+            measures[f"{m}_{d}_wrong"] = wrong
+    return measures
+
+def plot_measures(measures_dict, scores, colors, labels, ax = None, title=""):
+    if ax == None:
+        fig, ax = plt.subplots(figsize=(12,12), tight_layout=True)
+    colors = colors[:len(scores)]
+    for score, color, label in zip(scores, colors, labels):
+        sns.distplot(x=measures_dict[score], ax=ax, rug=True, kde=True, hist=False, 
+                    kde_kws={"fill": True}, color=color, label=label)
+    ax.set_xlabel("Uncertainty Measure")
+    ax.axvline(0, color="black", linestyle="dashed")
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.set_xlim(-0.05, 0.4)
+    ax.set_title(title, fontsize=18)
+    ax.legend()
+    try:
+        return fig
+    except UnboundLocalError:
+        pass
